fb2ea296d974004f8388e8a49b964c4677ddb88f
Marco Antonio Firmino de Sousa <marco.volare@gmail.com>
2013-03-07 17:33:57 -0300

    Link direto para o chat via whoisonline. Visualização do curso na página de quem está online.

--- main/inc/lib/social.lib.php
+++ main/inc/lib/social.lib.php
@@ -766,7 +766,7 @@ class SocialManager extends UserManager {
             
             $html .= '<ul id="online_grid_container" class="thumbnails">';            
             foreach ($user_list as $uid) {
-                $user_info = api_get_user_info($uid);
+                $user_info = api_get_user_info($uid, true);
 				
 				/*
 				 * Alteração para link direto para o chat e identificação do curso do usuário. Pedido do acadêmico: Francisco Lima. 
@@ -776,6 +776,8 @@ class SocialManager extends UserManager {
 				 */
 				$campo = UserManager::get_extra_user_data_by_field($user_info['user_id'], "curso");
 				$user_info['curso'] = $campo['curso'];
+				$user_friend_relation = SocialManager::get_relation_between_contacts(api_get_user_id(), $uid);
+/*
 				$friends_id = SocialManager::get_friends($uid,"","", false);
 				$friends = array();
 				foreach ($friends_id as $f) {
@@ -784,7 +786,7 @@ class SocialManager extends UserManager {
 				$friend = in_array(api_get_user_id(), $friends);
 				$user_info['friend'] = $friend;
 				// Fim da alteração.
-				
+	*/			
 				  
                 //Anonymous users can't have access to the profile
                 if (!api_is_anonymous()) {
@@ -809,9 +811,26 @@ class SocialManager extends UserManager {
                 } else {
                     $friends_profile = UserManager::get_picture_user($uid, $image_array['file'], 80, USER_IMAGE_SIZE_ORIGINAL);                                        
                     $img = '<img title = "'.$name.'" alt="'.$name.'" src="'.$friends_profile['file'].'">';                                                                        
-                }           
-                $name = '<a href="'.$url.'">'.$status_icon.$user_status.$name.'</a><br>';
-                $html .= '<li class="span'.($column_size/3).'"><div class="thumbnail">'.$img.'<div class="caption">'.$name.'</div</div></li>';                
+                }
+				if($user_friend_relation != USER_RELATION_TYPE_FRIEND)
+				{      
+                	$name = '<a href="'.$url.'">'.$status_icon.$user_status.$name.'</a><br>';
+				} else
+				{
+					if (api_is_global_chat_enabled()) {
+					    if ($user_id != api_get_user_id()) {
+					            $options = array('onclick' => "javascript:chatWith('".$uid."', '".Security::remove_XSS($name)."', '".$user_info['user_is_online_in_chat']."')");
+					            $chat_icon = $status_icon; 
+					            $chat .=   Display::url($chat_icon.'&nbsp;&nbsp;',  'javascript:void(0);', $options);
+								$name = $chat . '<a href="'.$url.'">'.$user_status.$name.'</a><br>';
+					    }
+					} else {
+						$name = '<a href="'.$url.'">'.$status_icon.$user_status.$name.'</a><br>';
+					}
+									
+				}
+				
+                $html .= '<li class="span'.($column_size/3).'"><div class="thumbnail">'.$img.'<div class="caption">'. $name. $user_info['curso'] . '</div></div></li>';                
             }            
             $counter = $_SESSION['who_is_online_counter'];
             
