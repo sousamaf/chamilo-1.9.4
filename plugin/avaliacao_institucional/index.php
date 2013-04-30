a9fd17f5a20a58273443ac25f0d1472f75187ec7
Marco Antonio Firmino de Sousa <marco.volare@gmail.com>
2013-04-30 08:15:59 -0300

    Ajuste ao novo endereço da biblioteca survey.lib.php.

--- plugin/avaliacao_institucional/index.php
+++ plugin/avaliacao_institucional/index.php
@@ -7,20 +7,24 @@ require_once api_get_path(LIBRARY_PATH).'catolicaDoTocantins.lib.php';
 require_once api_get_path(LIBRARY_PATH).'avaliacao_institucional.lib.php';
 
 $user_id = api_get_user_id();
-if(true)//$user_id == '3')
+
+if($user_id > 2)
 {
 	$cpf = CatolicaDoTocantins::ct_getCpfFromUserid($user_id);
-	echo '<div class="well">';
-    echo "<h2>Avaliação Institucional</h2>";  
 	
-	//Using get_lang inside a plugin
-	if(AvaliacaoInstitucional::isActiveTeacher($cpf)){
-		echo get_lang('TratamentoDocente');
-	} else {
-		echo get_lang('TratamentoDiscente');
+	if( (AvaliacaoInstitucional::isActiveTeacher($cpf) && !AvaliacaoInstitucional::isSurveyDone($user_id, ENQUETEPROFESSOR)) OR ((AvaliacaoInstitucional::isActiveStudent($cpf) && !AvaliacaoInstitucional::isAllSurveyDone($user_id, $cpf)))){
+		echo '<div class="well">';
+	    echo "<h2>Avaliação Institucional</h2>";  
+		
+		//Using get_lang inside a plugin
+		if(AvaliacaoInstitucional::isActiveTeacher($cpf)){
+			echo get_lang('TratamentoDocente');
+		} else {
+			echo get_lang('TratamentoDiscente');
+		}
+		echo get_lang('BemvindoMensagem');
+		echo '<a href='.api_get_path(WEB_CODE_PATH).'avaliacao_institucional/index.php>'.get_lang('LinkAvaliacao').'</a>';
+		echo "<br>" . get_lang('Organizacao');
+		echo '</div>';
 	}
-	echo get_lang('BemvindoMensagem');
-	echo '<a href='.api_get_path(WEB_CODE_PATH).'avaliacao_institucional/index.php>'.get_lang('LinkAvaliacao').'</a>';
-	echo "<br>" . get_lang('Organizacao');
-	echo '</div>';
 }
